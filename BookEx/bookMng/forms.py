7cece7aba7bccc5f7fd36931bc0a61f1747900fd
AngelV129 <angelvillalobos1995@gmail.com>
2022-04-11 18:51:50 -0700

    added databases

---
+++ BookEx/bookMng/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Book
+
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = [
+            'name',
+            'web',
+            'price',
+            'picture',
+        ]
+
+
